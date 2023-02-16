9aad5e329584b5eea461cc4d48907a73c34d6ea6
Marcus Manuel Feliciano <marcus@feliciano.eu>
2023-02-16 17:06:57 +0100

    adasdadas

--- httpserver/examplesocketclient.js
+++ httpserver/examplesocketclient.js
@@ -3,13 +3,16 @@
 const io = require('socket.io-client')
 const socket = io('http://localhost:3000');
 
+//Definer hva brukernavnet vårt er og hva svaret vårt er
 let brukernavn = 'moskito'
 let svar = '3asdasdasd'
 
 
+//Koble seg til socket serveren.
 socket.on('connect', () => {
     console.log('connected')
 })
 
+//Send infromasjonen i et objekt som inneholder både brukernavn og svar, med identifikatoren 'usr&response'.
 socket.emit('usr&response', {brukernavn, svar})
 
